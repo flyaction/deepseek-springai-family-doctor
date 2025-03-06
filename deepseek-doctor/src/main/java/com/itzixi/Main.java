a9e25587fc9369f75128e680abcadb74a5497be2
action <xingdong1117@126.com>
2025-03-06 16:21:42 +0800

    feat:项目初始化

---
+++ deepseek-doctor/src/main/java/com/itzixi/Main.java
@@ -0,0 +1,11 @@
+package com.itzixi;
+
+/**
+ * @author: action
+ * @create: 2025/3/6 16:16
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
